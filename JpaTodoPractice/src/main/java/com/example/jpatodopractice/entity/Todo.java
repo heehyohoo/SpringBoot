0651464098aae5f50df6db79dc0f1b076073a1a8
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-03 21:01:13 +0900

    FEAT: Entity 클래스 작성

--- JpaTodoPractice/src/main/java/com/example/jpatodopractice/entity/Todo.java
+++ JpaTodoPractice/src/main/java/com/example/jpatodopractice/entity/Todo.java
@@ -1,7 +1,32 @@
 package com.example.jpatodopractice.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "todo")
 public class Todo {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "todos")
+    private String todos;
+
+    @Column(name = "indate")
+    private String indate;
 
 }
