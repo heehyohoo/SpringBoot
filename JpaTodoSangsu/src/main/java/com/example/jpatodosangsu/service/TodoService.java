0aeb1c8673935c5a86dd3ea2c3987bdc7ebe6248
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-03 19:35:50 +0900

    FEAT : 이번에는 서비스를 인터페이스로 생성, 리포지토리 세팅

--- JpaTodoSangsu/src/main/java/com/example/jpatodosangsu/service/TodoService.java
+++ JpaTodoSangsu/src/main/java/com/example/jpatodosangsu/service/TodoService.java
@@ -1,7 +1,19 @@
 package com.example.jpatodosangsu.service;
 
+import com.example.jpatodosangsu.dto.TodoResDTO;
+
+import java.util.List;
+
 public interface TodoService {
 
 
+    //할일 목록 불러오기
+    List<TodoResDTO> todolist();
+
+    //할일 삭제
+    String deleteTodos(String id);
+
+    //할일 등록
+    String insertTodos(String todos);
 
 }
