b621892ed71447ed635120d996245da505f1cd5c
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-03 16:20:08 +0900

    FEAT:JPA 연습2 service 설정완료

--- jpaPractice2/src/main/java/com/example/jpapractice2/service/ExamService.java
+++ jpaPractice2/src/main/java/com/example/jpapractice2/service/ExamService.java
@@ -1,7 +1,21 @@
 package com.example.jpapractice2.service;
 
+import com.example.jpapractice2.repository.ExamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExamService {
+
+    //모든 로직은 서비스에서 이루어진다.
+
+    ExamRepository examRepository;
+
+    @Autowired
+    public ExamService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+
+
 }
