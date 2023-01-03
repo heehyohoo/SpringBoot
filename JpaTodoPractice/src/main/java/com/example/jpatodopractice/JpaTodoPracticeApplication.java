acd0b6eb542e242622fe999ff11407c5d4a73851
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-03 20:55:49 +0900

    FEAT: index 파일 추가

---
+++ JpaTodoPractice/src/main/java/com/example/jpatodopractice/JpaTodoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpatodopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTodoPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTodoPracticeApplication.class, args);
+    }
+
+}
---
+++ JpaTodoPractice/src/test/java/com/example/jpatodopractice/JpaTodoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpatodopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTodoPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
