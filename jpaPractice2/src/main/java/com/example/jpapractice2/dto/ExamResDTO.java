7260087c524b4baef88812c3c467f1a181dd64d5
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-03 16:43:33 +0900

    FEAT:JPA 연습2 안에서 응답하여 나가는 DTO 생성

---
+++ jpaPractice2/src/main/java/com/example/jpapractice2/dto/ExamResDTO.java
@@ -0,0 +1,15 @@
+package com.example.jpapractice2.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ExamResDTO {      //안에서 밖으로 나가는 응답 DTO
+
+    private String name;
+
+}
