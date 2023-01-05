ffe9dadf348baa873ddd429e76e8474dd6d225c7
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-05 17:49:10 +0900

    FEAT: 필요 dto 3가지 구현

---
+++ jpa-calendarPractice02/src/main/java/com/example/jpacalendarpractice02/dto/ScheduleRequest.java
@@ -0,0 +1,32 @@
+package com.example.jpacalendarpractice02.dto;
+
+import com.example.jpacalendarpractice02.entity.Schedule;
+import com.example.jpacalendarpractice02.entity.User;
+import lombok.*;
+
+import javax.persistence.Table;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class ScheduleRequest {
+
+    private String email;
+    private Long id;
+    private String title;
+    private String date;
+
+
+    public Schedule toEntity(ScheduleRequest scheduleRequest) {
+        return Schedule.builder()
+                .user(User.builder().email(email).build())
+                .id(id)
+                .title(title)
+                .date(date).build();
+    }
+
+}
