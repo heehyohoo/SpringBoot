20ccaa119e697bb725e24ff382bedc939b096f4b
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-09 20:30:57 +0900

    FEAT: AOP 를 활용한  logging 구현

--- jpa-calendarPractice02/src/main/java/com/example/jpacalendarpractice02/aop/LogAdvice.java
+++ jpa-calendarPractice02/src/main/java/com/example/jpacalendarpractice02/aop/LogAdvice.java
@@ -18,7 +18,7 @@ public class LogAdvice {
 
     //컨트롤러, 서비스, 리포짓 토리를 지나면 아래와같이 로그를 기록한다.
     @Around(" execution(* com.example.jpacalendarpractice02.controller..*Controller.*(..))" +
-            "||  execution(* com.example.jpacalendarpractice02.service..*Controller.*(..))" +
+            "|| execution(* com.example.jpacalendarpractice02.service..*Controller.*(..))" +
             "|| execution(* com.example.jpacalendarpractice02.repository..*Controller.*(..))")
     public Object logAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         String className = proceedingJoinPoint.getSignature().getDeclaringTypeName(); // 클래스이름
@@ -26,9 +26,10 @@ public class LogAdvice {
         String parameters = Arrays.toString(proceedingJoinPoint.getArgs()); // 파라미터
 
 
-        logger.info("[[START]]"+className+"."+proceedingJoinPoint.getSignature().getName()+"() <=================");
-        logger.info("Argument/Parameter : "+ Arrays.toString(proceedingJoinPoint.getArgs()));//<-파라미터
-        logger.info("================[[END : "+proceedingJoinPoint.getSignature().getName()+"()]]==================");
+        logger.info("===========================[[START]]===========================");
+        logger.info(""+className+"."+methodName+"() CALLED");
+        logger.info("PARAMETERS : " + parameters);
+        logger.info("===========================[[END]]===========================");
 
 
 
