feb88dc81b3ef138348ce12c717e902cb46d11a6
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-10 14:33:34 +0900

    3번째 JPA 튜토리얼 연습!

---
+++ jpa-calendarPractice03/src/main/java/com/example/jpacalendarpractice03/JpaCalendarPractice03Application.java
@@ -0,0 +1,13 @@
+package com.example.jpacalendarpractice03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCalendarPractice03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCalendarPractice03Application.class, args);
+    }
+
+}
---
+++ jpa-calendarPractice03/src/test/java/com/example/jpacalendarpractice03/JpaCalendarPractice03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpacalendarpractice03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCalendarPractice03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
