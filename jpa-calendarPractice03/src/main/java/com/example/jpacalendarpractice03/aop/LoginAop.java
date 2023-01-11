5baf121f903c38c5aa2854211b61dd02d6058710
JangJaeHack(Jay) <myride.jay@gmail.com>
2023-01-11 19:44:10 +0900

    로그인 aop를 적용하여 세션에 email이 없을경우 로그아웃버튼을 눌러도 반응이 없게 만듬.

--- jpa-calendarPractice03/src/main/java/com/example/jpacalendarpractice03/aop/LoginAop.java
+++ jpa-calendarPractice03/src/main/java/com/example/jpacalendarpractice03/aop/LoginAop.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.w3c.dom.ls.LSOutput;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,14 +17,16 @@ import javax.servlet.http.HttpSession;
 @Component
 public class LoginAop {
 
+
+
     @Pointcut("execution(* com.example.jpacalendarpractice03.controller.*ScheduleController.*(..))")
     public void cut() {
 
     }
-//    @Pointcut("execution(* com.example.coinadmin.admin.controller.*AdminRestController.*(..))")
-//    public void cut2() {
-//
-//    }
+    @Pointcut("execution(* com.example.jpacalendarpractice03.page.controller.PageController.logout(..))")
+    public void cut2() {
+
+    }
 
     @Around("cut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -37,16 +40,16 @@ public class LoginAop {
     }
 
 
-//    @Around("cut2()")
-//    public Object around2(ProceedingJoinPoint joinPoint) throws Throwable {
-//        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
-//
-//        if (session.getAttribute("Id") == null) { // 로그인을 했을때
-//            return null;
-//        } else { // 로그인을 안했을때
-//            return joinPoint.proceed();
-//        }
-//    }
+    @Around("cut2()")
+    public Object around2(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+
+        if (session.getAttribute("email") == null) { // 로그인을 했을때
+            return "redirect:/";
+        } else { // 로그인을 안했을때
+            return joinPoint.proceed();
+        }
+    }
 }
 
 
--- jpa-calendarPractice03/src/main/java/com/example/jpacalendarpractice03/page/controller/PageController.java
+++ jpa-calendarPractice03/src/main/java/com/example/jpacalendarpractice03/page/controller/PageController.java
@@ -2,7 +2,10 @@ package com.example.jpacalendarpractice03.page.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class PageController {
@@ -25,5 +28,15 @@ public class PageController {
     @GetMapping("/goCalendar")
     public String goCalendar() {
         return "calendar";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session, Model model) {
+
+        session.setAttribute("email",null);
+        model.addAttribute("message","로그아웃 성공");
+        model.addAttribute("url","/");
+
+        return "message";
     }
 }
