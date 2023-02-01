ee580b9a2dbb0177b32f82c6bae5fe80df4aa3d8
heehyohoo <myride.jay@gmail.com>
2023-02-01 17:41:30 +0900

    aws

--- jpa-calendarPractice01/src/test/java/com/example/jpacalendarpractice01/JpaCalendarPractice01ApplicationTests.java
+++ jpa-calendarPractice01/src/test/java/com/example/jpacalendarpractice01/JpaCalendarPractice01ApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.jpacalendarpractice01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class JpaCalendarPractice01ApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
